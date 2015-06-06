536d2942742acd7a012504433dafb92baf8fdf9d
Anuresh Kumar <anureshkravi@gmail.com>
2015-06-06 18:36:22 +0530

    Changes in main file.

--- js/common.js
+++ js/common.js
@@ -4,4 +4,13 @@ $(window).load(function(){
 	$("#it:checked");
 	
 	});
+	
+$('#selectall').click(function(){ 
+     $(':checkbox').prop("checked", true);
+	 alert("All item selected.");
+});
 
+$('#selectall').click(function(){ 
+     $(':checkbox').prop("checked", false);
+	 alert("All item deselect.");
+});
